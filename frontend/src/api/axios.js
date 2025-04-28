a35932d5b7001f6d3f3d5fe06b0d797fc8dbe9cd
nancylama <nncylama@gmail.com>
2025-04-28 12:51:14 -0400

    m

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://thriving-peace-production.up.railway.app',
-    withCredentials: true,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  withCredentials: true,
 });
 
 export default api;
