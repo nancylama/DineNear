b00687502151ae0d372a3c982ce4493616c6b0f3
KJ Moses <kjm7832@nyu.edu>
2025-04-29 13:29:16 -0400

    Create authMiddleWare.js

---
+++ backend/authMiddleWare.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader?.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, 'your_secret_key', (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+function authorizeRoles(...allowedRoles) {
+  return (req, res, next) => {
+    if (!req.user || !allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({ error: 'Forbidden' });
+    }
+    next();
+  };
+}
+
+module.exports = { authenticateToken, authorizeRoles };
